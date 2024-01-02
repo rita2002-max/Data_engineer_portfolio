7153ca5ad8f7c0a2ca139a7d43a60bb1af2ec74d
Rita NTOLO <81691610+rita2002-max@users.noreply.github.com>
2024-01-02 10:48:42 +0100

    New files uploaded

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+var hiddenElements = document.querySelectorAll('.hidden');
+var skillElements = document.querySelectorAll('.skill');
+var hiddenLeftElements = document.querySelectorAll('.hidden-left');
 
 function opentab(tabname) {
   for (tablink of tablinks) {
@@ -11,3 +14,30 @@ function opentab(tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show")
+    }
+    // else {
+    //   entry.target.classList.remove("show")
+    // }
+  })
+});
+
+const observerSkill = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("skill-show")
+    }
+    else {
+      entry.target.classList.remove("skill-show")
+    }
+  })
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+skillElements.forEach((el) => observerSkill.observe(el));
